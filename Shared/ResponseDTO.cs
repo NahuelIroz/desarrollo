5c5d592458ad0ff10983f187b2117542eb1a2762
NahuelIroz <82222142+NahuelIroz@users.noreply.github.com>
2024-02-02 19:10:36 -0300

    Add files via upload

---
+++ Shared/ResponseDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorEcommerce.Shared
+{ //ES LA RESPUESTA A TODAS LAS SOLICITUDES QUE VAMOS A HACER DESDE LA API
+    public class ResponseDTO<T>
+    {
+        public T? Resultado { get; set; }
+        public bool EsCorrecto { get; set; }
+        public string? Mensaje { get; set; }
+    }
+}
